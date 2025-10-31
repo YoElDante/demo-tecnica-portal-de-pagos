eac35e1d18720b004b33f4b47349fcd78be7ba74
YoElDante <vivayodante@gmail.com>
2025-10-31 11:14:04 -0300

    chore: se vuelve a colocar require('dotenv').config(); en /config/database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -19,11 +19,13 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
+  port: process.env.DB_PORT || 1433,
+  dialect: process.env.DB_DIALECT || 'mssql',
   dialectOptions: {
     options: {
       encrypt: true, // obligatorio para Azure
       trustServerCertificate: false, // true si no se usa SSL
+      hostNameInCertificate: '*.database.windows.net' // verificar si con esto funciona bien
     }
   },
   logging: true // opcional, false para no mostrar logs de SQL
